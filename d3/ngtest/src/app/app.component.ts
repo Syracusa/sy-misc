bde70dd8ecb9be6cc2f5bc6b909a5bcd6f24a6c1
kj <msrndsy@gmail.com>
2022-12-29 06:39:27 +0900

    Parse data from json files

--- d3/ngtest/src/app/app.component.ts
+++ d3/ngtest/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import * as d3 from 'd3';
-import * as moment from 'moment'
+import moment from 'moment'
 
-class tdata {
-  day: string = "";
-  asMoment: moment.Moment = moment();
-  locs: Map<string, number> = new Map<string, number>;
+import DataInfo from '../assets/datainfo.json';
 
-
-}
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,50 +14,84 @@ class tdata {
 })
 export class AppComponent {
 
+  data : any[] = [];
+  asMoment: moment.Moment[] = [];
+  datasize : number = 0;
+  readDone : number = 0;
+  VERBOSE = 0;
+
   ngOnInit(): void {
     console.log('ngoninit!!');
-    this.drawGraph();
+
+    console.log(DataInfo);
+
+    this.datasize = DataInfo['files'].length
+    for (let i = 0; i < this.datasize; i++) {
+      this.getJSON('assets/' + DataInfo['files'][i]).subscribe(data => {
+        
+        if (this.VERBOSE){
+          console.log(DataInfo['files'][i].split('.')[0])
+          console.log(data);
+        }
+
+        this.data.push(data['All']);
+
+        /* Check missing data */
+        for (let eidx = 0; eidx < DataInfo['extensions'].length; eidx++){
+          let ext = DataInfo['extensions'][eidx];
+          if (!(ext in data['All'])){
+            data['All'][ext] = 0;
+          }
+        }
+
+        this.asMoment.push(moment(DataInfo['files'][i].split('.')[0]));
+        this.readDone += 1;
+
+        if (this.readDone == this.datasize){
+          console.log(this.data);
+          console.log(this.asMoment);
+          this.drawGraph();
+        }
+      });
+    }
+  }
+
+  constructor(private http: HttpClient) {}
+
+  public getJSON(filename: string): Observable<any> {
+    return this.http.get(filename);
   }
 
   drawGraph(): void {
-    let data = [
-      { 'c': 120, 'cpp': 80, 'rs': 100 },
-      { 'c': 130, 'cpp': 82, 'rs': 105 },
-      { 'c': 166, 'cpp': 215, 'rs': 110 },
-      { 'c': 170, 'cpp': 230, 'rs': 115 },
-      { 'c': 200, 'cpp': 240, 'rs': 150 }
-    ];
-
-    let asMoment: moment.Moment[] = [
-      moment('2022-12-21'),
-      moment('2022-12-22'),
-      moment('2022-12-23'),
-      moment('2022-12-24'),
-      moment('2022-12-25')
-    ];
-
-    var colors = ['red', 'green', 'blue'];
+    var colors = ['#001122', '#002233', '#003344', '#004455', 
+                  '#005566', '#006677', '#007788', '#008899',
+                  '#0099AA', '#00AABB', '#00BBCC', '#00CCDD'];
 
     /* Stack Data Generator */
     var stackGen = d3.stack()
-      .keys(['c', 'cpp', 'rs']);
+      .keys(DataInfo.extensions);
 
     /* Generate Data */
-    var stackedSeries = stackGen(data);
+    var stackedSeries = stackGen(this.data);
+
+    console.log(stackedSeries);
+
+    let maxDate = moment.max(this.asMoment);
+    let minDate = moment.min(this.asMoment);
 
     /* Scale */
     var xScale = d3.scaleTime()
       .domain(
         [
           /* Give some margin */
-          asMoment[0].clone().add(-6, 'hours'),
-          asMoment[4].clone().add(6, 'hours')
+          minDate.clone().add(-6, 'hours'),
+          maxDate.clone().add(6, 'hours')
         ]
       )
       .range([50, 600]);
 
     var yScale = d3.scaleLinear()
-      .domain([0, 700])
+      .domain([0, 20000])
       .range([450, 50])
       .nice();
 
@@ -81,8 +112,8 @@ export class AppComponent {
         return d;
       })
       .join('rect')
-      .attr("x", function (d, i) {
-        return xScale(asMoment[i]) - (barWidth / 2);
+      .attr("x",  (d, i) => {
+        return xScale(this.asMoment[i]) - (barWidth / 2);
       })
       .attr("y", function (d, i) {
         return yScale(d[1]);
@@ -101,9 +132,9 @@ export class AppComponent {
       xAxis = d3.axisBottom(xScale);
     } else {
       xAxis = d3.axisBottom(xScale).ticks(5)
-        .tickFormat(function (d, i) {
+        .tickFormat((d, i) => {
           console.log('format');
-          return asMoment[i].format('YY-MM-DD');
+          return this.asMoment[i].format('YY-MM-DD');
         });
     }
 
