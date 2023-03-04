4102ad6bd116a0972b1d0fe0f26d7c3396d50d72
kj <msrndsy@gmail.com>
2023-03-04 14:08:17 +0900

    Update

--- react/test1/src/App.js
+++ react/test1/src/App.js
@@ -1,9 +1,59 @@
 import './App.css';
 
+import React from "react"
+
+let gameState = {
+  turn: 1
+}
+
+class PlayerScoreSheet extends React.Component {
+  render() {
+    return (
+      <>
+        <div> Test scoresheet </div>
+        <table>
+          <thead>
+            <tr>
+              <th colspan="2"> Your Name </th>
+            </tr>
+            <tr>
+              <th>Tactic</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> For of a kind  </td>
+              <td> test body 2 </td>
+            </tr>
+            <tr>
+              <td> test body 3 </td>
+              <td> test body 4 </td>
+            </tr>
+          </tbody>
+        
+        </table>
+
+      </>
+
+
+    );
+  }
+}
+
+function Board(){
+  return (
+    <></>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      Test1
+      Turn : Player  {gameState.turn}
+      
+      <PlayerScoreSheet></PlayerScoreSheet>
+      <PlayerScoreSheet></PlayerScoreSheet>
     </div>
   );
 }
