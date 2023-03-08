d0a8e90fac16d9dc0d119e342fcaef8344133bea
kj <msrndsy@gmail.com>
2023-03-08 14:53:18 +0900

    Add minimal camctrl

--- three/test.js
+++ three/test.js
@@ -5,16 +5,17 @@ import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/Orb
 import { FlyControls } from 'https://unpkg.com/three/examples/jsm/controls/FlyControls.js';
 
 import { FirstPersonControls } from 'https://unpkg.com/three/examples/jsm/controls/FirstPersonControls.js';
- 
+
 // scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xf0f0f0 );
+scene.background = new THREE.Color(0xf0f0f0);
 
 
-let camPos = {x:0, y:6, z:10};
+let camPos = { x: 0, y: 10, z: 10 };
 // camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(camPos.x,camPos.y,camPos.z);
+camera.position.set(camPos.x, camPos.y, camPos.z);
+camera.lookAt(0,0,0);
 
 
 // renderer
@@ -27,12 +28,9 @@ renderer.shadowMap.type = THREE.PCFShadowMap;
 document.body.appendChild(renderer.domElement);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-// orbit controls
-// const controls = new FirstPersonControls(camera, renderer.domElement);
-
 // plane geometry
 const geometryP = new THREE.PlaneGeometry(100, 100);
-const materialP = new THREE.MeshStandardMaterial({color:0xffffff})
+const materialP = new THREE.MeshStandardMaterial({ color: 0xffffff })
 const plane = new THREE.Mesh(geometryP, materialP);
 
 plane.castShadow = false;
@@ -43,22 +41,22 @@ scene.add(plane);
 // sphere geometry
 const geometryS = new THREE.SphereGeometry(2, 32, 32);
 const materialS = new THREE.MeshStandardMaterial({
-    color:0xffffff,
+    color: 0xffffff,
     wireframe: true,
 });
 
 const sphere = new THREE.Mesh(geometryS, materialS);
 
-let spherePos = {x:0, y:3, z:0};
+let spherePos = { x: 0, y: 3, z: 0 };
 sphere.position.set(spherePos.x, spherePos.y, spherePos.z);
 sphere.castShadow = true;
 scene.add(sphere);
 
-let lightPos = {x:2, y:10, z:1};
+let lightPos = { x: 2, y: 10, z: 1 };
 
 const geometrylightSource = new THREE.SphereGeometry(1, 8, 8);
 const materiallightSource = new THREE.MeshStandardMaterial({
-    color:0xffffff
+    color: 0xffffff
 });
 
 const lightSource = new THREE.Mesh(geometrylightSource, materiallightSource);
@@ -72,22 +70,89 @@ light.target.position.set(0, 0, 0);
 light.castShadow = true;
 scene.add(light);
 
+
+// Control
+let wPressed = 0;
+let dPressed = 0;
+let aPressed = 0;
+let sPressed = 0;
+
+window.onkeydown  = (e) => {
+    console.log(e.key + ' pressed');
+    switch (e.key) {
+        case 'w':
+            wPressed = 1;
+            break;
+        case 'a':
+            aPressed = 1;
+            break;
+        case 's':
+            sPressed = 1;
+            break;
+        case 'd':
+            dPressed = 1;
+            break;
+    }
+}
+window.onkeyup  = (e) => {
+    console.log(e.key + ' up');
+    switch (e.key) {
+        case 'w':
+            wPressed = 0;
+            break;
+        case 'a':
+            aPressed = 0;
+            break;
+        case 's':
+            sPressed = 0;
+            break;
+        case 'd':
+            dPressed = 0;
+            break;
+    }
+}
+
+/*
+canvasElem.onmousedown = function (e) {
+    console.log("Mouse down pos : " + e.clientX + ", " + e.clientY + "");
+}
+*/
+
+let oldTime = Date.now();
+
+function inputControl(timeDiff)
+{
+    if (wPressed) {
+        camPos.z -= 0.00001 * timeDiff;
+    }
+    if (sPressed) {
+        camPos.z += 0.00001 * timeDiff;
+    }
+    if (aPressed) {
+        camPos.x -= 0.00001 * timeDiff;
+    }
+    if (dPressed) {
+        camPos.x += 0.00001 * timeDiff;
+    }
+    
+}
+
 // rendering function
-function animate() {	
+function animate() {
     requestAnimationFrame(animate);
 
+    let timeDiff = Date.now() - oldTime;
 
-    spherePos.x += 0.01;
-    lightPos.x += 0.02;
+    inputControl(timeDiff);
+    // spherePos.x += 0.00001 * timeDiff;
+    // lightPos.x += 0.00002 * timeDiff;
+
     sphere.position.set(spherePos.x, spherePos.y, spherePos.z);
     light.position.set(lightPos.x, lightPos.y, lightPos.z);
     lightSource.position.set(lightPos.x, lightPos.y, lightPos.z);
-    
-    camera.lookAt(spherePos.x, spherePos.y, spherePos.z);
 
-    // camPos.x += 0.01;
-    // camera.position.set(camPos.x,camPos.y,camPos.z);
-
+    // camera.lookAt(spherePos.x, spherePos.y, spherePos.z);
+    camera.position.set(camPos.x,camPos.y,camPos.z);
 
     // controls.update();
     renderer.render(scene, camera);
